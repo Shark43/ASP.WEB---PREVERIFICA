b4cbc59d2316a262e6edefb4f38cbe12d32fbecb
Shark43 <talia4343@gmail.com>
2019-03-24 20:41:27 +0100

    commenti

--- ClassLibraryAccessoAiDati/DataList.cs
+++ ClassLibraryAccessoAiDati/DataList.cs
@@ -14,7 +14,9 @@ namespace ClassLibraryAccessoAiDati {
         public static BindingList<Studenti> Studentis { get; set; } = new BindingList<Studenti>();
         public static BindingList<Classe> Classes { get; set; } = new BindingList<Classe>();
 
+        //stringa di connessione per access
         //public static string connectionString { get; } = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + AppDomain.CurrentDomain.GetData("DataDirectory") + "\\per_verifica.accdb";
+        //stringa di connesione per sql server
         public static string connectionString { get; } = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + AppDomain.CurrentDomain.GetData("DataDirectory") + "\\test.mdf";
 
         //Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\Scuola\informatica\ASP\PerVerifica\WebApplicationAPI\App_Data\per_verifica.accdb
